4bb01c2fd621e42d569ad93fb33222fe070e9bec
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2024-05-06 19:44:13 -0300

    fix: finish primitive types

--- exercises/primitive_types/primitive_types3/main.go
+++ exercises/primitive_types/primitive_types3/main.go
@@ -1,12 +1,12 @@
 // primitive_types3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-
+	who := "Felipe"
+	country := "Brazil"
 	fmt.Printf("Hello, I am %s and live in %s\n", who, country)
 }
