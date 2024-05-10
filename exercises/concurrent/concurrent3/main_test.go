4120ee249f0b3f3f8dc68e832eef24d95657d4c8
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2024-05-10 19:26:05 -0300

    fix: finish concurrent

--- exercises/concurrent/concurrent3/main_test.go
+++ exercises/concurrent/concurrent3/main_test.go
@@ -1,7 +1,6 @@
 // concurrent3
 // Make the tests pass!
 
-// I AM NOT DONE
 package main_test
 
 import (
@@ -33,11 +32,8 @@ func sendAndReceive(buf *bytes.Buffer, messages chan string) {
 
 	greeting := <-messages
 	fmt.Fprint(buf, greeting)
-
-	// Here we just receive the first message
-	// Consider using a for-range loop to iterate over the messages
-	_, ok := <-messages
-	if !ok {
-		fmt.Fprint(buf, "Channel is closed")
+	for greeting = range messages {
+		fmt.Fprint(buf, " ")
+		fmt.Fprint(buf, greeting)
 	}
 }
